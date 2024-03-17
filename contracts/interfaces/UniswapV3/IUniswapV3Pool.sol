6810ac08217f8c1f07c21e02bfa63bad13849ab8
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2024-03-17 23:35:39 +0800

    remove files

--- contracts/interfaces/UniswapV3/IUniswapV3Pool.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
-
-interface IUniswapV3Pool {
-
-  function liquidity() external view returns(uint128);
-  function fee() external view returns(uint24);
-}
