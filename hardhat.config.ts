56b5bfabeb700b7fb9bd3124174c322bb7a9288f
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2024-06-19 02:48:16 +0800

    add sample contract and test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,22 @@
-import '@nomicfoundation/hardhat-ethers';
-import '@nomicfoundation/hardhat-verify';
-import 'hardhat-deploy';
-import 'hardhat-deploy-ethers';
-import 'dotenv/config';
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import "dotenv/config";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-
   solidity: {
-    version: "0.8.17",
+    version: "0.8.20",
     settings: {
       viaIR: true,
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
 
   networks: {
@@ -26,27 +25,26 @@ export default {
       chainId: 1,
       accounts: process.env.KEYS_PROD!.split(","),
       timeout: 120000000,
-      zksync: false
+      zksync: false,
     },
     goerli: {
       url: process.env.GOERLI_PROVIDER,
       chainId: 5,
       accounts: process.env.KEYS_DEV!.split(","),
       timeout: 120000000,
-      zksync: false
-    }
+      zksync: false,
+    },
   },
-  
+
   etherscan: {
     apiKey: {
       mainnet: process.env.ETHERSCAN_APIKEY,
-      goerli: process.env.ETHERSCAN_APIKEY
-    }
+      goerli: process.env.ETHERSCAN_APIKEY,
+    },
   },
 
   defender: {
     apiKey: process.env.DEFENDER_TEAM_API_KEY,
     apiSecret: process.env.DEFENDER_TEAM_API_SECRET_KEY,
-  }
-  
-}
+  },
+};
